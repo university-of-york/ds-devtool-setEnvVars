41bf7dbaf60e46eb77197076c257138a0230a06f
Ben Briggs <ben.briggs@york.ac.uk>
2023-11-02 11:21:35 +0000

    chore(dev): Move prettier config to package.json

--- scripts/move-git-tag.js
+++ scripts/move-git-tag.js
@@ -7,11 +7,11 @@ const moveGitTag = async () => {
     if (!version) {
         throw new Error(`Could not find major version from ${version}.`);
     }
-    
+
     const tagName = `v${version}`;
 
     console.warn(`Updating ${tagName} tag to latest commit...`);
-    
+
     await execa('git', ['push', '--delete', 'origin', tagName]);
     await execa('git', ['tag', '-f', tagName]);
     await execa('git', ['push', 'origin', '--tags']);
